28c129abfe7a03449ce7a9e99ca3e95f81247a0b
Julia Tsapova <12tsapova@gmail.com>
2019-03-04 04:39:43 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+if(screen.width >= 1024) {
+  $('.more-hidden').addClass('desktop-more');
+  $('.kran .desktop-more').css('border-radius', '0');
+  $('.gruz-evakuator .desktop-more').css('border-radius', '0');
+  $('.gruz-evakuator .desktop-more').css('background-color', '#fff');
+}
+
+$('.more').click(function(event) {
+  // event.preventDefault();
+  // console.log($(event.target).parent().find('.more-hidden'))
+  
+  $(this).next().slideDown(1000);
+});
+
+var scrollToElem = function(fromElem, toElem) {
+  fromElem.on( 'click', function(){ 
+    var dest = toElem; // получаем направление
+    if(dest !== undefined && dest !== '') { // проверяем существование
+        $('html').animate({ 
+            scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+        }, 1000 // скорость прокрутки
+        );
+    }
+    return false;
+});
+};
+
+scrollToElem($('.scroll-kran'), $('.kran'));
+scrollToElem($('.scroll-evakuator'), $('.gruz-evakuator'));
+scrollToElem($('.scroll-samogruz'), $('.samogruz'));
+
